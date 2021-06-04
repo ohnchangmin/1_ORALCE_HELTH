1a41ba06f29f61b797cabd0ff9149cfa877653de
mobey3 <mobey1818@gmail.com>
2021-06-04 12:42:15 +0900

    form9

--- 헬스장~2/Form3.cs
+++ 헬스장~2/Form3.cs
@@ -16,7 +16,7 @@ namespace 헬스장프로그램
         {
             InitializeComponent();
         }
-
+        private string gender;
         private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
         {
 
@@ -29,7 +29,24 @@ namespace 헬스장프로그램
 
         private void button6_Click(object sender, EventArgs e)
         {
-           
+            String name = Convert.ToString(t_name.Text);
+            String address = Convert.ToString(t_address.Text);
+            String tel = Convert.ToString(t_tel.Text);
+            String period = Convert.ToString(t_period.Text);
+            String day = Convert.ToString(t_day.Text);
+            String rocker = Convert.ToString(t_rocker.Text);
+
+            Form9 showFrom9 = new Form9();
+            showFrom9.Show();
+
+            showFrom9.label1.Text = ("이름: " + name + "\n" +
+                                     "성별: " + gender + "\n" +
+                                     "주소: " + address + "\n" +
+                                     "연락처: " + tel + "\n" +
+                                     "등록 기간: " + period + "\n" +
+                                     "등록일: " + day + "\n" +
+                                     "락커룸: " + rocker + "\n");
+
         }
 
         private void button7_Click(object sender, EventArgs e)
@@ -42,6 +59,16 @@ namespace 헬스장프로그램
         private void label5_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void man_CheckedChanged(object sender, EventArgs e)
+        {
+            gender = "남자";
+        }
+
+        private void woman_CheckedChanged(object sender, EventArgs e)
+        {
+            gender = "여자";
         }
     }
 }
